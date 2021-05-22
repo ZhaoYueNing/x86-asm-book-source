b8bc1f519d29f4b5c3a8f3a92f2c5bac4f742619
ZhaoYueNing <zhaozhaozhao1996@163.com>
2021-05-22 16:09:18 +0800

    change charset

--- c08/c08_mbr.asm
+++ c08/c08_mbr.asm
@@ -11,10 +11,10 @@ SECTION mbr align=16 vstart=0x7c00
          ;设置堆栈段和栈指针 
          mov ax,0      
          mov ss,ax
-         mov sp,ax
-      
+         mov sp,ax                        ;为何ss:sp = 0x000000 不是从高地址向低地址发展吗
+         
          mov ax,[cs:phy_base]            ;计算用于加载用户程序的逻辑段地址 
-         mov dx,[cs:phy_base+0x02]
+         mov dx,[cs:phy_base+0x02]       ;计算30位除法 0x10000 ÷ 16 商在 AX 中
          mov bx,16        
          div bx            
          mov ds,ax                       ;令DS和ES指向该段以进行操作
@@ -23,14 +23,14 @@ SECTION mbr align=16 vstart=0x7c00
          ;以下读取程序的起始部分 
          xor di,di
          mov si,app_lba_start            ;程序在硬盘上的起始逻辑扇区号 
-         xor bx,bx                       ;加载到DS:0x0000处 
+         xor bx,bx                       ;加载到DS:0x0000处 DS:BX
          call read_hard_disk_0
       
          ;以下判断整个程序有多大
          mov dx,[2]                      ;曾经把dx写成了ds，花了二十分钟排错 
          mov ax,[0]
          mov bx,512                      ;512字节每扇区
-         div bx
+         div bx                          ;DX-AX / 512 = 所需加载的扇区数量
          cmp dx,0
          jnz @1                          ;未除尽，因此结果比实际扇区数少1 
          dec ax                          ;已经读了一个扇区，扇区总数减1 
@@ -82,7 +82,7 @@ read_hard_disk_0:                        ;从硬盘读取一个逻辑扇区
          push ax
          push bx
          push cx
-         push dx
+         push d  x
       
          mov dx,0x1f2
          mov al,1
